6cf54f5a688afba4de0319739d7dec9fda439159
Keith Kenneally <keithkenneally08@gmail.com>
2018-06-28 16:31:29 +0100

    a message from remote repo

--- Testing/src/TestMain.java
+++ Testing/src/TestMain.java
@@ -1,16 +1,1 @@
-
-public class TestMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Testing");
-		
-		System.out.println("change made");
-
-				System.out.println("Third message");
-
-		
-	}
-
-}
+hello from remote repo
